e053fcdad46839916c33e374ae026f73ee6cd70e
BiagioCaputo <scebiagio98@gmail.com>
2024-05-18 20:23:10 +0200

    Router folder created

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+// Import delle pagine
+
+// import HomePage from '../assets/pages/HomePage.vue';
+
+
+//Rotte
+const router = createRouter({
+    history: createWebHistory(),
+    linkExactActiveClass: 'active-link',
+    routes: [
+        // { path: '/', component: HomePage, name: 'home' },
+    ]
+});
+
+export { router }
